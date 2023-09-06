32b66b12730286b1dad0799c8c036e8d40d55ddc
Shamim2601 <shamim2601@gmail.com>
2023-09-06 19:43:01 +0600

    update

--- src/DataStructure/LinkedList.java
+++ src/DataStructure/LinkedList.java
@@ -1,29 +1,28 @@
 package DataStructure;
 
 public class LinkedList {
-    public static void main(String[] args) {
+    private Node Head;
 
-        // arr = { 2, 6, 8, 12, 45 }
-        // adding and removing is tedious
-
-        Node Head = new Node(2); // (2)
-        Node Node1 = new Node(6);  // (2)  (6)
-        Head.setNext(Node1); // (2)->(6)
-
-        Node Node2 = new Node(8); // (2)->(6)   (8)
-        Node1.setNext(Node2);  // (2)->(6)->(8)
-
-        printLL(Head);  // (2)->(6)->(8)
-
-        int val = 12;
-        addVal(Head, val);
-
-        printLL(Head); // (2)->(6)->(8)->(12)
+    public LinkedList(){
+        Head = null;
     }
 
-    private static void addVal(Node head, int val) {
+    public LinkedList(int val) {
+        Node node = new Node(val);
+        Head = node;
+    }
+
+    public Node getHead() {
+        return Head;
+    }
+
+    public void setHead(Node head) {
+        Head = head;
+    }
+
+    public void addValAtLast(int val) {
         Node newNode = new Node(val);
-        Node currentNode = head; // (2)
+        Node currentNode = Head; // (2)
         while(currentNode.getNext()!=null){ // (6), (8), null
             currentNode = currentNode.getNext(); // (6), (8)
         }
@@ -31,9 +30,20 @@ public class LinkedList {
         currentNode.setNext(newNode);
     }
 
-    private static void printLL(Node head) {
-        System.out.print("The linked list: (" + head.getValue()+")"); // (2)
-        Node currentNode = head; // (2)
+    public void addValSorted(int val) {
+        Node newNode = new Node(val);  // 7
+        Node currentNode = Head; // (2)
+        while(currentNode.getNext().getValue()<newNode.getValue()){ // 2->6->7   ->8
+            currentNode = currentNode.getNext();
+        }
+        Node nextNode = currentNode.getNext();
+        currentNode.setNext(newNode);
+        newNode.setNext(nextNode);
+    }
+
+    public void printLL() {
+        System.out.print("The linked list: (" + Head.getValue()+")"); // (2)
+        Node currentNode = Head; // (2)
         while(currentNode.getNext()!=null){ // (6), (8), null
             System.out.print("->("+currentNode.getNext().getValue()+")"); // ->(6)->(8)
             currentNode = currentNode.getNext(); // (6), (8)
