79da11ea3a2c88163d4001e9f021167d1a21b152
Shamim2601 <shamim2601@gmail.com>
2023-09-06 19:43:10 +0600

    update

---
+++ src/DataStructure/Main.java
@@ -0,0 +1,13 @@
+package DataStructure;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Linked List");
+
+        LinkedList LL = new LinkedList(3);
+
+        LL.addValAtLast(7);
+
+        LL.printLL();
+    }
+}
